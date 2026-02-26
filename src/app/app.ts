7af0eea2e273710f17d1738aa789ab65a23f0caf
Zmrrr11 <zamirmarca90@gmail.com>
2026-02-25 20:29:53 -0400

    commit inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Avatar } from './components/avatar/avatar';  
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('practica-2');
+  title = 'practica-2';
 }
